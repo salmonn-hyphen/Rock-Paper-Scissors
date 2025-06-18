c783b989e5e1ada2c67d0dd5915136c75cb25e25
Phyoe Thiha <cybon149@gmail.com>
2025-06-18 12:53:10 +0630

    Rewrite Logic and Add UI

--- js/index.js
+++ js/index.js
@@ -1,60 +1,83 @@
 let playerScore = 0;
 let computerScore = 0;
-
+const restart = document.querySelector(".restart"); 
+const announcement = document.querySelector(".announcement");
 // get the Computer Choice
 let getComputerChoice = (num = 3) => {
-    let number= Math.floor(Math.random() * num);
-    switch (number){
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors";
-        default: return "Invalid Choice";
-    }
+  let number = Math.floor(Math.random() * num);
+  switch (number) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissor";
+    default:
+      return "Invalid Choice";
+  }
 };
 
-//get the Player Choice
-let getPlayerChoice = () => {
-    let choice = prompt("Enter your choice (Rock, Paper, Scissors):");
-    if (choice.trim === "" || choice === null) {
-        alert("You must enter a choice!");
-        return getPlayerChoice();
-    }
-    choice = choice.trim();
-    choice = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-    return choice;
-}
-
 // determine the winner of the single round
-let playRound = (playerChoice, computerChoice) => {
-    if (playerChoice === computerChoice){
-        alert("It is a tie! Both Chose " + playerChoice);
-    }else if((playerChoice === "Rock" && computerChoice === "Scissors") ||
-              (playerChoice === "Paper" && computerChoice === "Rock") ||
-              (playerChoice === "Scissors" && computerChoice === "Paper")) {
-        playerScore++;
-        alert("You win! " + playerChoice + " beats " + computerChoice);
+const clickEvent = document.querySelectorAll("img");
+clickEvent.forEach(img=>{ 
+  let result="";
+  img.addEventListener('click',()=>{
+    const playerChoice = img.id;
+    const computerChoice = getComputerChoice();
+    if (playerChoice === computerChoice) {
+      result=`Tie! Both Choose ${playerChoice}`;
+    } else if (
+      (playerChoice === "Rock" && computerChoice === "Scissor") ||
+      (playerChoice === "Paper" && computerChoice === "Rock") ||
+      (playerChoice === "Scissor" && computerChoice === "Paper")
+    ) {
+      playerScore++;
+      result=`Player Win! ${playerChoice} beats ${computerChoice}`;
     } else {
-        computerScore++;
-        alert("You lose! " + computerChoice + " beats " + playerChoice);
+      computerScore++;
+      result=`Computer Win! ${computerChoice} beats ${playerChoice}`;
     }
-}
-
-// play the game for 5 rounds and display the final score
-let playGame = () =>{
-    for (let i= 0 ; i < 5 ; i++){
-        const playerChoice = getPlayerChoice();
-        const computerChoice = getComputerChoice();
-        playRound(playerChoice, computerChoice);
-    }
-    alert("Final Score: Player " + playerScore + " - Computer " + computerScore);
-    if( playerScore === computerScore) {
-        alert("The game ends in a tie!");
-    }else if(playerScore > computerScore) {
-        alert("Congratulations! You win the game!");
-    } else if(playerScore < computerScore) {
-        alert("Sorry! You lose the game!");
-    }
-}
-document.addEventListener("DOMContentLoaded", () => {
-    playGame();
+    announcement.textContent = result;
+    const comScore = document.querySelector(".comScore");
+    comScore.textContent = computerScore;
+    const plScore = document.querySelector(".score .playerScore");
+    plScore.textContent = playerScore;
+    finalWinner();
+  }); 
 });
+
+//determine the final winner and disable the action
+const finalWinner = () =>{
+  if(playerScore >= 5 && computerScore <=  5){
+    announcement.textContent = `Congratulation! You Win The Game!`
+    announcement.setAttribute("style","color:green;");
+    const imgs = document.querySelectorAll("img");
+    imgs.forEach(img => img.setAttribute("style", "pointer-events: none;"));
+    showBtn();
+  }else if(playerScore <= 5 && computerScore >= 5){
+    announcement.textContent = `Sorry! You Lose The Game, Try Again!`
+    announcement.setAttribute("style","color:red;");
+    clickEvent.forEach(img => img.setAttribute("style", "pointer-events: none;"));
+    showBtn(); 
+  }
+ }
+
+ //show restart button and it's action
+ const showBtn = () => {
+  //appear button with final result
+  const btn = document.createElement("button");
+  btn.textContent = "Play Again";
+  restart.append(btn);
+
+  //restart game when click button
+  btn.addEventListener('click',()=>{
+    btn.remove();
+    playerScore = 0;
+    computerScore = 0;
+    document.querySelector(".score .playerScore").textContent = playerScore;
+    document.querySelector(".score .comScore").textContent = computerScore;
+    document.querySelector(".announcement").textContent="";
+    clickEvent.forEach(img => img.setAttribute("style", "pointer-events: auto;"));
+    announcement.setAttribute("style", "color:black;")
+  });
+};
